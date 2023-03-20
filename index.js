28108671e4d68d9b51c88e967f0cd38623ca9a27
gabr-silva <gabrielvieira.s0801@gmail.com>
2023-03-20 20:04:51 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//importar o express
+const express = require ('express');
+
+//instanciar express
+const app = express();
+
+app.use (express.json);
+
+
+//criação do servidor HTTP
+app.listen(3000, ()=>{
+    console.log('servidor em teste');
+});
